a598d2b8806a7e54b55e8b1a8a56399828c73f96
Jean-Philippe <jp.desmet1@gmail.com>
2020-05-13 21:02:06 +0200

    #15 Les commentaires sont biens affichés en dessous du billet

--- controller/postController.php
+++ controller/postController.php
@@ -1,6 +1,7 @@
 <?php 
 require_once('model/Manager.php');
 require_once('model/billetsManager.php');
+require_once('model/commentManager.php');
 
 function listBillets()
 {
@@ -14,6 +15,8 @@ function billet()
 {
     $billetManager = new Projet4\Blog\Billet\Billet();//Création de l'objet
     $billet = $billetManager->getBillet($_GET['id']);
+    $commentManager = new \Projet4\Blog\commentManager\commentManager();//Création de l'objet
+    $comments = $commentManager->getComments($billet['id']);//Appel d'une fonction de cet objet
 
     require('view/viewBillet.php');
 }
--- model/commentManager.php
+++ model/commentManager.php
@@ -3,14 +3,13 @@ namespace Projet4\Blog\commentManager;//Classe dans ce namespace
 
 require_once('model/manager.php');
 
-class commentManager extends manager
+class commentManager
 {
     public function getComments($id_billet)
     {
         $db = new \Projet4\Blog\Manager\Manager();
         $bdd = $db->dbConnect();
-        $comments = $bdd->prepare('SELECT id, author, content, date_content, DATE_FORMAT(date_content \'%d\%m\Y à %Hh%imin%ss\')
-        AS date_content_fr FROM comments WHERE id_billet = ? ORDER BY date_content DESC');
+        $comments = $bdd->prepare('SELECT id, author, content, date_content FROM comments WHERE id_billet = ? ORDER BY date_content DESC');
         $comments->execute(array($id_billet));
 
         return $comments;
